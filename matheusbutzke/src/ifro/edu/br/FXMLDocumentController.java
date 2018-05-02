06be575c3d7e1ed8767c0d45510b4bf270bb3b9e
Matheus Butzke <matheusbutzke99@gmail.com>
2018-05-02 10:49:39 -0400

    Fazendo calculadora

--- matheusbutzke/src/ifro/edu/br/FXMLDocumentController.java
+++ matheusbutzke/src/ifro/edu/br/FXMLDocumentController.java
@@ -11,25 +11,69 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author 02119149240
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private TextField txtNumero1;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField txtNumero2;
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    private void soma(ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 + num2;
+
+        txtResultado.setText(resultado.toString());
     }
-    
+
+    @FXML
+    private void subtracao(ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 - num2;
+
+        txtResultado.setText(resultado.toString());
+    }
+
+    @FXML
+    private void divisao(ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 / num2;
+
+        txtResultado.setText(resultado.toString());
+    }
+
+    @FXML
+    private void multiplicacao(ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 * num2;
+
+        txtResultado.setText(resultado.toString());
+    }
+
+    @FXML
+    private void limpar(ActionEvent event) {
+
+        txtResultado.setText("");
+        txtNumero1.setText("");
+        txtNumero2.setText("");
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
+
