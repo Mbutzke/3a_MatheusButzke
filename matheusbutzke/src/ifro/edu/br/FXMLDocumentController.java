f433e258578f8b888eede220f0fa7eb9c2758394
Matheus Butzke <matheusbutzke99@gmail.com>
2018-05-02 14:12:55 -0400

    Criando Vinculo com Banco de dados, criando tela de casdastro aluno, e menu principal

--- matheusbutzke/src/ifro/edu/br/FXMLDocumentController.java
+++ matheusbutzke/src/ifro/edu/br/FXMLDocumentController.java
@@ -5,13 +5,18 @@
  */
 package ifro.edu.br;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -23,8 +28,21 @@ public class FXMLDocumentController implements Initializable {
     private TextField txtNumero1;
     @FXML
     private TextField txtNumero2;
+
     @FXML
     private TextField txtResultado;
+    @FXML
+    private Label label;
+    @FXML
+    private JFXButton btnSoma;
+    @FXML
+    private JFXButton btnSubtratir;
+    @FXML
+    private JFXButton btnMultiplicar;
+    @FXML
+    private JFXButton btnDividir;
+    @FXML
+    private JFXButton btnLimpar;
 
     @FXML
     private void soma(ActionEvent event) {
@@ -75,5 +93,24 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }
 
-}
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
 
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 900, 682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar aluno");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+
+        }
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    }
+
+}
